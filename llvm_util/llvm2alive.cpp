93dbc5c390106921e903077d7ac5e80920c0b784
Can <can.lehmann@emmtrix.com>
2024-08-06 19:36:49 +0200

    Support msvc

--- llvm_util/llvm2alive.cpp
+++ llvm_util/llvm2alive.cpp
@@ -2123,7 +2123,7 @@ public:
       const char *chrs = name.data();
       char *end_ptr;
       auto numeric_id = strtoul(chrs, &end_ptr, 10);
-      if (end_ptr != name.end())
+      if (end_ptr != &*name.end())
         return M->getGlobalVariable(name, true);
       else {
         auto itr = M->global_begin(), end = M->global_end();
